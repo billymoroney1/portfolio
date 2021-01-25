210666efee8cc177fb0b5e3c919d1cf2fafeadfa
billymoroney <YOUR-EMAIL-ADDRESS>
2021-01-25 13:46:08 -0600

    improved mobile exp

--- components/Layout.js
+++ components/Layout.js
@@ -22,11 +22,11 @@ export default function Layout({children}){
                 </div> 
             </nav>
             
-            <div className='w-full opacity-85 bg-cyan-100 flex flex-col items-center justify-around text-xl p-20'>
+            <div className='w-full opacity-85 bg-cyan-100 flex flex-col items-center justify-around text-xl lg:p-5'>
                 {/* <h1 className='font-extrabold text-center text-4xl pb-6'>About Me</h1> */}
                     <img src='http://placekitten.com/200/200' className='rounded-xl' />
                     <hr className='border-12 border-gray-900 w-3/5 mt-7'></hr>
-                    <p className='md:w-3/5 text-black p-8 '>I'm a full-stack software engineer in Chicago with a background in music, writing, and teaching. I fell in love with coding because it gives me opportunities to solve difficult problems and be creative every day.</p>
+                    <p className='sm:w-4/5 md:w-3/5 text-black p-8'>I'm a full-stack software engineer in Chicago with a background in music, writing, and teaching. I fell in love with coding because it gives me opportunities to solve difficult problems and be creative every day.</p>
                     <hr className='border-12 border-gray-900 w-3/5'></hr>
                     <p className='md:w-2/5 text-black p-8'>I coded some Java in high school, fell in love with visual/audio programming tools like Max and Reactor, and was motivated by those interests to learn full-stack development at General Assembly.</p>
                     <hr className='border-12 border-gray-900 w-3/5'></hr>
