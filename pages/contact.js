3a270c30483f48512376bbf1a4400b142d32c023
billymoroney <YOUR-EMAIL-ADDRESS>
2021-01-25 14:31:00 -0600

    navbar consistency

--- pages/contact.js
+++ pages/contact.js
@@ -3,23 +3,23 @@ import Form from '../components/Form.js'
 
 export default function Contact() {
     return (
-        <div>
+        <div className="font-sans bg-cyan-300 font-manrope font-light">
             <nav className='bg-cyan-300 text-gray-700 shadow-lg'>
-                        <div className="container mx-auto">
-                            <div className="flex justify-around">
-                            <a className="text-3xl font-bold p-3">billy moroney</a>
-                            <ul className="space-x-10 sm:self-center text-xl border-t sm:border-none">
-                                <li className="hover:text-green-500 inline">
-                                    <Link href='/'>home</Link>
-                                </li>
-                                <li className="hover:text-green-400 inline">
-                                    <Link href='/contact'>contact</Link>
-                                </li>
-                            </ul>
-                                
-                        </div>
-                    </div> 
-                </nav>
+                    <div className="container mx-auto">
+                        <div className="flex justify-around">
+                        <a className="text-3xl font-bold p-3">billy moroney</a>
+                        <ul className="space-x-10 sm:self-center text-xl sm:border-none">
+                            <li className="hover:text-rose-500 inline border-4 border-gray-600 rounded-xl p-2">
+                                <Link href='/'>home</Link>
+                            </li>
+                            <li className="hover:text-rose-500 inline border-4 border-gray-600 rounded-xl p-2">
+                                <Link href='/contact'>contact</Link>
+                            </li>
+                        </ul>
+                            
+                    </div>
+                </div> 
+            </nav>
                 <Form />
             </div>
     )
