03d1fcbf71aac328f705a1f38ffbb15fb694cf5a
billymoroney <YOUR-EMAIL-ADDRESS>
2021-02-01 13:39:27 -0600

    fixed some spacing for small screens

--- components/Layout.js
+++ components/Layout.js
@@ -32,7 +32,7 @@ export default function Layout({children}){
             
             <div className='w-full opacity-85 bg-cyan-100 flex flex-col space-y-32 items-center justify-around text-xl'>
                 {/* <h1 className='font-extrabold text-center text-4xl pb-6'>About Me</h1> */}
-                    <p data-aos="fade-up" className='sm:w-4/5 md:w-3/5 text-black md:text-3xl p-32'>I'm a full-stack software engineer in Chicago with a background in music, writing, and teaching. I fell in love with coding because it gives me opportunities to solve difficult problems and be creative every day.</p>
+                    <p data-aos="fade-up" className='sm:w-4/5 md:w-3/5 text-black md:text-3xl p-8 pt-32 pb-32'>I'm a full-stack software engineer in Chicago with a background in music, writing, and teaching. I fell in love with coding because it gives me opportunities to solve difficult problems and be creative every day.</p>
                     <div data-aos="fade-up" className="w-full h-40 bg-rose-600 text-gray-100 shadow-lg flex items-center justify-around align-middle font-extrabold text-center text-4xl">
                     <span>About Me</span>
                     </div>
@@ -43,7 +43,7 @@ export default function Layout({children}){
                         <p className='text-black align-middle m-4'>I learned some Java in high school, started working extensively with visual/audio programming tools like Max and Reactor in college, and was motivated by those interests to learn full-stack development at General Assembly.</p>
                     </div>
                     <hr data-aos="fade-up" className='border-12 border-gray-900 w-3/5'></hr>
-                    <div data-aos="fade-up" className="md:grid md:gap-3 md:grid-cols-2 md:text-2xl w-3/5 md:m-12 justify-items-center items-center">
+                    <div data-aos="fade-up" className="md:grid md:gap-3 md:grid-cols-2 md:text-2xl w-3/5 md:m-12 justify-items-center items-center pb-32">
                         <p className='pt-8 pb-8 text-2xl text-center'>I also make <a href='https://billymoroney.bandcamp.com/releases' className='text-cyan-500' target='_blank'>music</a></p>
                         <img src='/assistant.jpeg' className='rounded-xl' style={{height: '300px', width: '275px'}} />
                     </div>
